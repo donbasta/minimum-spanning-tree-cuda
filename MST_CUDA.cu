83aa73592558905625364b09b6bd2e8587120f12
Farras Mohammad Hibban Faddila <13518017@std.stei.itb.ac.id>
2021-03-24 09:04:32 +0000

    Update MST_CUDA.cu

--- MST_CUDA.cu
+++ MST_CUDA.cu
@@ -1,9 +1,12 @@
+%%writefile MST_CUDA.cu
+
 #include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <math.h>
 #include <algorithm>
 #include <time.h>
+#include <string>
 
 using namespace std;
 
@@ -28,75 +31,94 @@ void make(int a, int b) {
     sz[a] += sz[b];
 }
 
-__global__ void sort_edges_by_weight(Edge *ar, Edge *temp, int len, int gap) {
-    int index = threadIdx.x;
-    int stride = blockDim.x;
-    
-    for (int le = index * gap;le < len;le += stride * gap) {
-        int i = le;
-        int mid = i + gap / 2;
-        if (mid >= len) continue;
-        int j = mid;
-        int ri = min(mid + gap / 2, len);
-
-        int cur = le;
-        while (i < mid && j < ri) {
-            bool compare = 0;
-            if (ar[i].w != ar[j].w) compare = (ar[i].w < ar[j].w);
-            else if (ar[i].a != ar[i].a) compare = (ar[i].a < ar[j].a);
-            else compare = (ar[i].b < ar[j].b);
-
-            if (compare)
-                temp[cur++] = ar[i++];
-            else
-                temp[cur++] = ar[j++];
-        }
-        while (i < mid) {
-            temp[cur++] = ar[i++];
-        }
-        while (j < ri) {
-            temp[cur++] = ar[j++];
-        }
-        for (int k = le;k < ri;k++) {
-            ar[k] = temp[k];
-        }
-    }
+__device__ int getIdx(dim3* threads, dim3* blocks) {
+    int x;
+    return threadIdx.x +
+           threadIdx.y * (x  = threads->x) +
+           threadIdx.z * (x *= threads->y) +
+           blockIdx.x  * (x *= threads->z) +
+           blockIdx.y  * (x *= blocks->z) +
+           blockIdx.z  * (x *= blocks->y);
 }
-__global__ void sort_edges_lexicographically(Edge* ar, Edge* temp, int len, int gap) {
-    int index = threadIdx.x;
-    int stride = blockDim.x;
 
-    for (int le = index * gap;le < len;le += stride * gap) {
-        int i = le;
-        int mid = i + gap / 2;
-        if (mid >= len) continue;
-        int j = mid;
-        int ri = min(mid + gap / 2, len);
+__device__ void merge_edges_by_weight(Edge* ar, Edge* temp, int le, int mid, int ri) {
+    int cur = le;
+    int i = le;
+    int j = mid;
+    while (i < mid && j < ri) {
+        bool compare = 0;
+        if (ar[i].w != ar[j].w) compare = (ar[i].w < ar[j].w);
+        else if (ar[i].a != ar[i].a) compare = (ar[i].a < ar[j].a);
+        else compare = (ar[i].b < ar[j].b);
 
-        int cur = le;
-        while (i < mid && j < ri) {
-            bool compare = 0;
-            if (ar[i].a != ar[i].a) compare = (ar[i].a < ar[j].a);
-            else compare = (ar[i].b < ar[j].b);
-
-            if (compare)
-                temp[cur++] = ar[i++];
-            else
-                temp[cur++] = ar[j++];
-        }
-        while (i < mid) {
+        if (compare)
             temp[cur++] = ar[i++];
-        }
-        while (j < ri) {
+        else
             temp[cur++] = ar[j++];
-        }
-        for (int k = le;k < ri;k++) {
-            ar[k] = temp[k];
-        }
+    }
+    while (i < mid) {
+        temp[cur++] = ar[i++];
+    }
+    while (j < ri) {
+        temp[cur++] = ar[j++];
     }
 }
 
-int main() {
+__device__ void merge_edges_lexicographically(Edge* ar, Edge* temp, int le, int mid, int ri) {
+    int cur = le;
+    int i = le;
+    int j = mid;
+    while (i < mid && j < ri) {
+        bool compare = 0;
+        if (ar[i].a != ar[j].a) compare = (ar[i].a < ar[j].a);
+        else compare = (ar[i].b < ar[j].b);
+        if (compare)
+            temp[cur++] = ar[i++];
+        else
+            temp[cur++] = ar[j++];
+    }
+    while (i < mid) {
+        temp[cur++] = ar[i++];
+    }
+    while (j < ri) {
+        temp[cur++] = ar[j++];
+    }
+}
+
+__global__ void sort_edges_by_weight(Edge *ar, Edge *temp, int len, int gap, int slices, dim3* threads, dim3* blocks) {
+    int index = getIdx(threads, blocks);
+    int le = gap * index * slices;
+
+    for (int slice = 0; slice < slices; slice++) {
+        if (le >= len) continue;
+
+        int mid = min(le + gap / 2, len);
+        int ri = min(le + gap, len);
+
+        merge_edges_by_weight(ar, temp, le, mid, ri);
+
+        le += gap;
+    }
+}
+
+__global__ void sort_edges_lexicographically(Edge *ar, Edge *temp, int len, int gap, int slices, dim3* threads, dim3* blocks) {
+    int index = getIdx(threads, blocks);
+    int le = gap * index * slices;
+
+    for (int slice = 0; slice < slices; slice++) {
+        if (le >= len) continue;
+
+        int mid = min(le + gap / 2, len);
+        int ri = min(le + gap, len);
+
+        merge_edges_lexicographically(ar, temp, le, mid, ri);
+
+        le += gap;
+    }
+}
+
+int main(int argc, char** argv) {
+
     cin >> n;
     par.resize(n);
     for (int i = 0;i < n;i++) {
@@ -110,40 +132,110 @@ int main() {
             if (w >= 0 && i < j) edges.emplace_back(w, i, j);
         }
     }
-    cudaSetDevice(0);
-    clock_t sttime = clock();
-    int len = edges.size();
-    Edge *ar, *temp;
-    cudaMallocManaged(&ar, len * sizeof(Edge));
-    cudaMallocManaged(&temp, len * sizeof(Edge));
-
-    for (int i = 0;i < len;i++) {
-        ar[i] = edges[i];
-    }
 
     int blockSize = 32;
-    for (int gap = 2;gap < len * 2;gap <<= 1) {
-        sort_edges_by_weight<<<1, blockSize>>>(ar, temp, len, gap);
-        cudaDeviceSynchronize();
+    if (argc > 1) {
+        blockSize = stoi(argv[1]);
     }
 
+    cudaSetDevice(0);
+    dim3 threadsPerBlock;
+    dim3 blocksPerGrid;
+
+    threadsPerBlock.x = 1;
+    threadsPerBlock.y = 1;
+    threadsPerBlock.z = 1;
+
+    blocksPerGrid.x = blockSize;
+    blocksPerGrid.y = 1;
+    blocksPerGrid.z = 1;
+
+    dim3* D_threads;
+    dim3* D_blocks;
+
+    cudaMallocManaged((void**) &D_threads, sizeof(dim3));
+    cudaMallocManaged((void**) &D_blocks, sizeof(dim3));
+
+    cudaMemcpy(D_threads, &threadsPerBlock, sizeof(dim3), cudaMemcpyHostToDevice);
+    cudaMemcpy(D_blocks, &blocksPerGrid, sizeof(dim3), cudaMemcpyHostToDevice);
+
+    int len = edges.size();
+    Edge *ar, *temp, *data;
+    cudaMalloc(&ar, len * sizeof(Edge));
+    cudaMalloc(&temp, len * sizeof(Edge));
+    data = (Edge*) malloc(len * sizeof(Edge));
+    for (int i = 0; i < len; i++) {
+        data[i] = edges[i];
+    }
+    cudaMemcpy(ar, data, len * sizeof(Edge), cudaMemcpyHostToDevice);
+
+    int numThreads = threadsPerBlock.x * threadsPerBlock.y * threadsPerBlock.z *
+                      blocksPerGrid.x * blocksPerGrid.y * blocksPerGrid.z;
+
+    Edge *ar_1 = ar;
+    Edge *temp_1 = temp;
+
+    clock_t sttime = clock();
+
+    cudaEvent_t start_sort_1, end_sort_1;
+    cudaEventCreate(&start_sort_1);
+    cudaEventCreate(&end_sort_1);
+
+    cudaEventRecord(start_sort_1);
+    for (int gap = 2;gap < len * 2;gap <<= 1) {
+        int slices = len / ((numThreads) * gap) + 1;
+        sort_edges_by_weight<<<blocksPerGrid, threadsPerBlock>>>(ar_1, temp_1, len, gap, slices, D_threads, D_blocks);
+        cudaDeviceSynchronize();
+        ar_1 = ar_1 == ar ? temp : ar;
+        temp_1 = temp_1 == ar ? temp : ar;
+    }
+    cudaEventRecord(end_sort_1);
+    cudaEventSynchronize(end_sort_1);
+    float time_sort_1 = 0;
+    cudaEventElapsedTime(&time_sort_1, start_sort_1, end_sort_1);
+
+
+    cudaMemcpy(data, ar_1, len * sizeof(Edge), cudaMemcpyDeviceToHost);
+    
     int ans = 0;
     Edge* ans_edges;
-    cudaMallocManaged(&ans_edges, len * sizeof(Edge));
+    ans_edges = (Edge*) malloc(len * sizeof(Edge));
     {
         int id = 0;
         for (int i = 0;i < len;i++) {
-            if (find(ar[i].a) == find(ar[i].b)) continue;
-            ans_edges[id++] = ar[i];
-            ans += ar[i].w;
-            make(ar[i].a, ar[i].b);
+            if (find(data[i].a) == find(data[i].b)) continue;
+            ans_edges[id++] = data[i];
+            ans += data[i].w;
+            make(data[i].a, data[i].b);
         }
         len = id;
     }
+    
+    cudaMemcpy(ar, ans_edges, len * sizeof(Edge), cudaMemcpyHostToDevice);
+
+    cudaEvent_t start_sort_2, end_sort_2;
+    cudaEventCreate(&start_sort_2);
+    cudaEventCreate(&end_sort_2);
+
+    ar_1 = ar;
+    temp_1 = temp;
+
+    cudaEventRecord(start_sort_2);
+
     for (int gap = 2;gap < len * 2;gap <<= 1) {
-        sort_edges_lexicographically<<<1, blockSize>>>(ans_edges, temp, len, gap);
+        int slices = len / ((numThreads) * gap) + 1;
+        sort_edges_lexicographically<<<blocksPerGrid, threadsPerBlock>>>(ar_1, temp_1, len, gap, slices, D_threads, D_blocks);
         cudaDeviceSynchronize();
+        ar_1 = ar_1 == ar ? temp : ar;
+        temp_1 = temp_1 == ar ? temp : ar;
     }
+    cudaEventRecord(end_sort_2);
+    cudaEventSynchronize(end_sort_2);
+    float time_sort_2 = 0;
+    cudaEventElapsedTime(&time_sort_2, start_sort_2, end_sort_2);
+
+    cudaMemcpy(ans_edges, ar_1, len * sizeof(Edge), cudaMemcpyDeviceToHost);
+
     clock_t endtime = clock();
     cout << ans << endl;
     for (int i = 0;i < len;i++) {
@@ -151,9 +243,15 @@ int main() {
     }
     cout << "Waktu Eksekusi: " << (endtime - sttime) * 1000 / CLOCKS_PER_SEC << " ms" << endl;
 
+    cout << "Waktu Eksekusi sorting 1 " << time_sort_1 << " ms" << endl;
+    cout << "Waktu Eksekusi sorting 2 " << time_sort_2 << " ms" << endl;
+
     cudaFree(ar);
     cudaFree(temp);
-    cudaFree(ans_edges);
+    cudaFree(D_threads);
+    cudaFree(D_blocks);
+    free(ans_edges);
+    free(data);
 
     return 0;
 }
